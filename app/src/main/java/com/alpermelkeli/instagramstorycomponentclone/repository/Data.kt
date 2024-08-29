2dc9548cd1b5589bcb6d47f90797401dbf5f6b9a
alpermelkeli <alpermelkeli@gmail.com>
2024-08-29 13:22:52 +0300

    Completed.

--- app/src/main/java/com/alpermelkeli/instagramstorycomponentclone/repository/Data.kt
+++ app/src/main/java/com/alpermelkeli/instagramstorycomponentclone/repository/Data.kt
@@ -2,13 +2,16 @@ package com.alpermelkeli.instagramstorycomponentclone.repository
 
 const val user1Id = "user1"
 const val user2Id = "user2"
-
+const val test_image_1 = "https://raw.githubusercontent.com/alpermelkeli/InstagramStoryComponentClone/master/tulips.png"
+const val test_image_2 = "https://raw.githubusercontent.com/alpermelkeli/InstagramStoryComponentClone/master/HappyFish.jpg"
+const val test_image_3 = "https://raw.githubusercontent.com/alpermelkeli/InstagramStoryComponentClone/master/sampleImage.jpg"
+// stories list : List<Pair<String,List<Story>>>
 val storiesList = listOf(
-    Pair(user1Id, listOf(Story("profilePhoto","user1","image"),
-        Story("profilePhoto","user1","image")
+    Pair(user1Id, listOf(Story(test_image_1,"user1", test_image_1),
+        Story(test_image_1,"user1", test_image_3)
     )),
     Pair(
-        user2Id, listOf(Story("profilePhoto","user1","image"),
-        Story("profilePhoto","user1","image")
+        user2Id, listOf(Story(test_image_2,"user2", test_image_3),
+        Story(test_image_2,"user2",test_image_1)
     )),
 )
--- app/src/main/java/com/alpermelkeli/instagramstorycomponentclone/screen/Main.kt
+++ app/src/main/java/com/alpermelkeli/instagramstorycomponentclone/screen/Main.kt
@@ -5,12 +5,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
 fun Main(onStoryClicked:()->Unit){
 
-    Box(Modifier.fillMaxSize()) {
+    Box(Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center) {
         Button(onClick = {onStoryClicked()}) {
             Text(text = "Go to stories.")
         }
